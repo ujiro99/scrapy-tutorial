b9ad430af0edc671a6a6f70e37596826c4ed5b37
ujiro99 <siro.cola@gmail.com>
2019-02-24 21:15:49 +0900

    add spiders.

---
+++ tutorial/spiders/quotes_spider.py
@@ -0,0 +1,22 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    def start_requests(self):
+        url = 'http://quotes.toscrape.com/'
+        tag = getattr(self, 'tag', None)
+        if tag is not None:
+            url = url + 'tag/' + tag
+        yield scrapy.Request(url, callback=self.parse)
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+            }
+
+        for a in response.css('li.next a'):
+            yield response.follow(a, callback=self.parse)
